63cc99f76a2893d7f80c5bac7d7082a41ae671e3
Filippo1808 <120219512+Filippo1808@users.noreply.github.com>
2022-12-13 15:49:13 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,5 +2,15 @@
 using namespace std;
 
 int main() {
+   FLOAT a=0, b=0, x=0, y=0, z=0;
+   cin >> a >> b >> x >> y;
+   if(x < 0 && y > 0){
+		z= a*x-b*y;
+	}elas if(x >= 0 && y <= 0){
+		z=a*x*x - b*y;
+	}elas{
+		z = a*x + b*y*y;
+	}
+	  cout << z;
    return 0;
 }
